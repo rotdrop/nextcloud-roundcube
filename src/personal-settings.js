7a6f41009040ad7c75aa1b4899934bd5ba5ff386
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-03-21 23:17:22 +0100

    Fix inconsistent lincense information; use Affero everywhere.

--- src/personal-settings.js
+++ src/personal-settings.js
@@ -1,22 +1,22 @@
 /**
- * nextCloud - RoundCube mail plugin
+ * Nextcloud RoundCube App.
  *
  * @author Claus-Justus Heine
- * @copyright 2020 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2020, 2021 Claus-Justus Heine <himself@claus-justus-heine.de>
  *
- * This library is free software; you can redistribute it and/or
+ * Nextcloud RoundCube App is free software: you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * Nextcloud RoundCube App is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * AFFERO GENERAL PUBLIC LICENSE for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Nextcloud RoundCube App. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 import { webPrefix } from './config.js';
