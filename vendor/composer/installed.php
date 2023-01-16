9e1d9f649872986b298c76dc8927662a16ecc1c6
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-01-16 17:51:42 +0100

    For convenience and easier installation from directory Github include into the git repo.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rotdrop/nextcloud-app-roundcube',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3d816804793ed7d4ba1220d0b2416a60ad99e010',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rotdrop/nextcloud-app-roundcube' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3d816804793ed7d4ba1220d0b2416a60ad99e010',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
