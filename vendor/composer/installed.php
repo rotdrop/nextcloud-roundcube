99d854782c3b8f35f9d40cec7ecda02d7d40436a
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-01-28 14:37:11 +0100

    Update assets

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'name' => 'rotdrop/nextcloud-app-roundcube',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '31007dca9b48d2b874a0c723a931f2f21a518938',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
+        'reference' => 'dc5216c3942ffdc1042d59e4f997a97928c093c0',
+        'name' => 'rotdrop/nextcloud-app-roundcube',
         'dev' => false,
     ),
     'versions' => array(
         'rotdrop/nextcloud-app-roundcube' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '31007dca9b48d2b874a0c723a931f2f21a518938',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
+            'reference' => 'dc5216c3942ffdc1042d59e4f997a97928c093c0',
             'dev_requirement' => false,
         ),
     ),
