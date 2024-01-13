6084ca75bc686163782ac9ab0af968c2b84a1a78
Gaspard d'Hautefeuille <github@dhautefeuille.eu>
2024-01-13 11:54:58 +0200

    Explicitly declare the properties $userId and $l

--- lib/Service/AuthRoundCube.php
+++ lib/Service/AuthRoundCube.php
@@ -70,6 +70,12 @@ class AuthRoundCube
   private $rcSessionId;
   private $rcSessionAuth;
 
+  /**
+   * Explicitly declare the properties.
+   */
+  private $userId;
+  private $l; // Assuming $l is for localization (IL10N)
+
   // phpcs:disable Squiz.Commenting.FunctionComment.Missing
   public function __construct(
     Application $app,
