9a402f4bf1cbd19f578bbeb848fe503f98d07c64
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-10 23:45:00 +0100

    Bump vue front-end to NC 28

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,7 @@ webpackConfig.plugins = webpackConfig.plugins.concat([
       'src/toolkit/util/ajax.js',
       'src/toolkit/util/dialogs.js',
       'src/toolkit/util/file-download.js',
+      'src/toolkit/util/file-node-helper.js',
       'src/toolkit/util/generate-url.js',
       'src/toolkit/util/jquery.js',
       'src/toolkit/util/on-document-loaded.js',
