a23e41c96703d55cfb2b916bac969a39b5ba510a
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-04-08 16:34:13 +0200

    git subrepo pull src/toolkit
    subrepo:
      subdir:   "src/toolkit"
      merged:   "4803952"
    upstream:
      origin:   "git@github.com:rotdrop/nextcloud-app-toolkit-js.git"
      branch:   "main"
      commit:   "4803952"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

---
+++ src/toolkit/util/nextcloud-sidebar-root.ts
@@ -0,0 +1,68 @@
+/**
+ * @author Claus-Justus Heine
+ * @copyright 2025 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { getCurrentInstance } from 'vue';
+import Vue from 'vue';
+import { NodeStatus } from '@nextcloud/files';
+import type { Node} from '@nextcloud/files';
+
+let currentInstance: null|Vue = null;
+
+type SidebarRoot = Vue & { node?: Node };
+
+let sidebarRoot: null|SidebarRoot = null;
+
+const busyNodes: Node[] = [];
+
+/**
+ * Find the root of the right sidebar in order to get access to
+ * interesting properties like the current Node-object.
+ */
+export const getSidebarRoot = () => {
+  let instance = getCurrentInstance()?.proxy || null;
+  if (sidebarRoot && currentInstance === instance) {
+    return sidebarRoot;
+  }
+  currentInstance = instance;
+  while (instance && instance.$parent && instance.$parent.$options.name !== 'SidebarRoot') {
+    instance = instance?.$parent;
+  }
+  sidebarRoot = instance || null;
+  return sidebarRoot;
+};
+
+export const getSidebarNode = () => {
+  const sidebarRoot = getSidebarRoot();
+  return sidebarRoot?.node || null;
+};
+
+export const setSidebarNodeBusy = (state: boolean = true) => {
+  const node = getSidebarNode();
+  if (node && state) {
+    node.status = NodeStatus.LOADING;
+    busyNodes.push(node);
+  } else {
+    for (const node of busyNodes) {
+      node.status = undefined;
+    }
+    busyNodes.splice(0, busyNodes.length);
+  }
+};
+
+export const clearSidebarNodeBusy = () => setSidebarNodeBusy(false);
