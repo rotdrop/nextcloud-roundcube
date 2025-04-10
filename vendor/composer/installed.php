78434a50b19c4fc8f3d5ba48ee7f9192a315b5e8
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-04-10 12:19:58 +0200

    Add assests for convenience stable branch.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rotdrop/nextcloud-app-roundcube',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '659f3b08e3378db92dff24adb4d082734af70a43',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rotdrop/nextcloud-app-roundcube' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '659f3b08e3378db92dff24adb4d082734af70a43',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
