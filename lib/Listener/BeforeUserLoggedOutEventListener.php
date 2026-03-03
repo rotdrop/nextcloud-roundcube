c1b0aa857f5f235541be671f46066fb343ad0055
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-03-03 11:18:13 +0100

    Switch from deprecated IAppContainer DI to Psr ContainterInterface

--- lib/Listener/BeforeUserLoggedOutEventListener.php
+++ lib/Listener/BeforeUserLoggedOutEventListener.php
@@ -27,7 +27,7 @@ use Throwable;
 
 use Psr\Log\LoggerInterface as ILogger;
 
-use OCP\AppFramework\IAppContainer;
+use Psr\Container\ContainerInterface;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\User\Events\BeforeUserLoggedOutEvent as HandledEvent;
@@ -42,7 +42,7 @@ class BeforeUserLoggedOutEventListener implements IEventListener
   const EVENT = HandledEvent::class;
 
   // phpcs:disable Squiz.Commenting.FunctionComment.Missing
-  public function __construct(protected IAppContainer $appContainer)
+  public function __construct(protected ContainerInterface $appContainer)
   {
   }
   // phpcs:enable Squiz.Commenting.FunctionComment.Missing
